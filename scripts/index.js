145ac084c1968da12586087602a3d99a6cce17f0
Tatyana Kora <tatjanakora@yandex.ru>
2023-03-30 15:08:15 +0300

    fix: внесены правки

--- scripts/index.js
+++ scripts/index.js
@@ -1,18 +1,18 @@
 let popup = document.querySelector('.popup');
 let openButton = document.querySelector('.profile__edit');
 let closeButton = document.querySelector('.popup__close');
-let formElement =  document.querySelector('.popup__edit');
+let formElement = document.querySelector('.popup__edit');
 let nameInput = document.querySelector('.popup__input_type_name');
 let jobInput = document.querySelector('.popup__input_type_job');
 let nameAuthor = document.querySelector('.profile__name');
 let jobAuthor = document.querySelector('.profile__job-text');
 
 
-function closePopup () {
+function closePopup() {
   popup.classList.remove('popup_opened');
 }
 
-function openPopup () {
+function openPopup() {
   popup.classList.add('popup_opened');
   nameInput.value = nameAuthor.textContent;
   jobInput.value = jobAuthor.textContent;
@@ -23,11 +23,11 @@ openButton.addEventListener('click', openPopup);
 closeButton.addEventListener('click', closePopup);
 
 
-function handleFormSubmit (evt) {
+function handleFormSubmit(evt) {
   evt.preventDefault();
   nameAuthor.textContent = nameInput.value;
   jobAuthor.textContent = jobInput.value;
-  closePopup ();
+  closePopup();
 }
 
 formElement.addEventListener('submit', handleFormSubmit); 
