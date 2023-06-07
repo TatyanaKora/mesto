12608cfa6f73a7aa23ec2c0960687464182ff554
Tatyana Kora <tatjanakora@yandex.ru>
2023-06-07 20:07:54 +0300

    fix: правки после второго ревью

--- src/components/PopupWithImage.js
+++ src/components/PopupWithImage.js
@@ -2,13 +2,13 @@ import { Popup } from "./popup.js";
 export default class PopupWithImage extends Popup {
   constructor(popup) {
     super(popup);
+	this._popupPhotoName = this._popup.querySelector(".popup__photo-name");
+	this._popupPhotoImage = this._popup.querySelector(".popup__photo-img");
   }
   open(data) {
-	  //убрала в index файле повторения
-    this._popup.querySelector(".popup__photo-name").textContent = data.name;
-    const image = this._popup.querySelector(".popup__photo-img");
-    image.src = data.link;
-    image.alt = data.name;
+    this._popupPhotoName.textContent = data.name;
+    this._popupPhotoImage.src = data.link;
+    this._popupPhotoImage.alt = data.name;
     super.open();
   }
 }
